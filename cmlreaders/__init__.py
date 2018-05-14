3725b67be8ad70857ffb8d9979ab1dcff300523f
Zach Duey <zachduey@gmail.com>
2018-05-14 16:02:37 -0400

    incremented version number

--- cmlreaders/__init__.py
+++ cmlreaders/__init__.py
@@ -3,5 +3,5 @@ from .path_finder import *  # noqa
 from .cmlreader import *
 from .readers import *
 
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 version_info = namedtuple("VersionInfo", "major,minor,patch")(*__version__.split('.'))
