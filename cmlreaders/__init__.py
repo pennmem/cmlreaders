57fa6d7fcb57ca20b92dbdc4c0c05323bb3ea35c
Zach Duey <zachduey@gmail.com>
2018-05-14 14:49:43 -0400

    made more classes available from top level

--- cmlreaders/__init__.py
+++ cmlreaders/__init__.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from .path_finder import *  # noqa
 from .cmlreader import *
+from .readers import *
 
 __version__ = '0.1.1'
 version_info = namedtuple("VersionInfo", "major,minor,patch")(*__version__.split('.'))
