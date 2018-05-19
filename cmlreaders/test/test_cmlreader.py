34521b4d5032f34dc8874c2242cac9eab20dc61b
Zach Duey <zachduey@gmail.com>
2018-05-19 16:29:46 -0400

    cmlreader now only defines load and load_proxy

--- cmlreaders/test/test_cmlreader.py
+++ cmlreaders/test/test_cmlreader.py
@@ -10,10 +10,11 @@ class TestCMLReader:
     @pytest.mark.rhino
     @pytest.mark.parametrize("file_type", [
         'voxel_coordinates', 'classifier_excluded_leads', 'jacksheet',
-        'good_leads', 'leads', 'electrode_coordinates', 'prior_stim_results'
+        'good_leads', 'leads', 'electrode_coordinates', 'prior_stim_results',
+        'target_selection_table'
     ])
     def test_load_from_rhino(self, file_type, rhino_root):
-        reader = CMLReader(subject="R1405E", localization='0',
+        reader = CMLReader(subject="R1405E", localization='0', experiment='FR1',
                            rootdir=rhino_root)
         reader.load(file_type)
 
@@ -26,3 +27,14 @@ class TestCMLReader:
     def test_load(self, file_type):
         reader = CMLReader(subject="R1405E", localization='0', experiment="FR1")
         reader.load(data_type=file_type[:-4], file_path=datafile(file_type))
+
+    @pytest.mark.parametrize("file_type", [
+        'voxel_coordinates', 'classifier_excluded_leads', 'jacksheet',
+        'good_leads', 'leads', 'electrode_coordinates', 'prior_stim_results',
+        'target_selection_table'
+    ])
+    def test_load_proxy(self, file_type):
+        reader = CMLReader(subject='R1405E', localization='0', experiment='FR1',
+                           session='0', montage='0')
+        reader_obj = reader.load_proxy(file_type, file_path=datafile(file_type))
+        assert type(reader_obj) == reader.readers[file_type]
