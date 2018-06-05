d083202dc7f7ef314ba0debfcfb54089b6b6aa91
Michael V. DePalatis <mike@depalatis.net>
2018-06-05 10:30:25 -0400

    Import get_data_index into top-level namespace
    Also note that the order of other imports matters.

--- cmlreaders/__init__.py
+++ cmlreaders/__init__.py
@@ -1,4 +1,8 @@
 from collections import namedtuple
+
+from .data_index import get_data_index  # noqa
+
+# n.b. the order below matters so as to avoid circular imports
 from .path_finder import PathFinder  # noqa
 from .readers import *  # noqa
 from .cmlreader import CMLReader  # noqa
