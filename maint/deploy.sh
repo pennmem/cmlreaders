7694e5f3dc231783dad33abadb57a47afabb3cab
Michael V. DePalatis <mike@depalatis.net>
2018-08-07 14:04:33 -0400

    Add deploy script and section

---
+++ maint/deploy.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# This script gets run on TravisCI to deploy new releases. It gets triggered
+# on tagged versions only, so make sure to tag when releasing!
+set -ex
+
+python maint/build.py
+anaconda -t $ANACONDA_TOKEN -u pennmem upload build/**/$PKG_NAME-*.tar.bz2
