0da00a0598b0e2db08a69eb598b88a771f5fe521
Michael V. DePalatis <mike@depalatis.net>
2018-08-17 15:31:02 -0400

    Actually update version number

--- cmlreaders/__init__.py
+++ cmlreaders/__init__.py
@@ -8,5 +8,5 @@ from .path_finder import PathFinder  # noqa
 from .readers import *  # noqa
 from .cmlreader import CMLReader  # noqa
 
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 version_info = namedtuple("VersionInfo", "major,minor,patch")(*__version__.split('.'))
