398ee4e0f401d35364d93fd80ecb6dffa6c11b1c
Michael V. DePalatis <mike@depalatis.net>
2018-08-20 11:07:16 -0400

    Fix typo

--- maint/build.py
+++ maint/build.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-
-from __future__ import print_function
-
 from argparse import ArgumentParser
 import glob
 import os
@@ -71,9 +68,9 @@ def convert():
 
 
 def upload():
-    """ Upload pre-built package to conda """
-    for pltform in ['linux-64', 'osx-64', 'win-32', 'win-64']:
-        files = glob.glob('build/{}/*.tar.bz2'.format(pltform))
+    """Upload pre-built package to conda."""
+    for platform in ['linux-64', 'osx-64', 'win-32', 'win-64']:
+        files = glob.glob('build/{}/*.tar.bz2'.format(platform))
         cmds = ['anaconda upload -u pennmem {}'.format(f) for f in files]
         for cmd in cmds:
             print(cmd)
