8651b983d385adb67a43a37e8de32c618737e24f
leond <lad258@cornell.edu>
2018-10-16 15:03:49 -0400

    incrementing version number for new build instead of build number now that we're deploying through travis

--- cmlreaders/__init__.py
+++ cmlreaders/__init__.py
@@ -8,5 +8,5 @@ from .path_finder import PathFinder  # noqa
 from .readers import *  # noqa
 from .cmlreader import CMLReader  # noqa
 
-__version__ = "0.9.5"
+__version__ = "0.9.5a"
 version_info = namedtuple("VersionInfo", "major,minor,patch")(*__version__.split('.'))
