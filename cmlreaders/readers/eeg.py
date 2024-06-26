8656593aed179b140bbd097bdbce1bdc3822de4b
hherrema <hherrema@sas.upenn.edu>
2024-06-26 15:34:43 -0400

    improve error message
    epoched v. un-epoched EEG

--- cmlreaders/readers/eeg.py
+++ cmlreaders/readers/eeg.py
@@ -295,9 +295,10 @@ class SplitEEGReader(BaseEEGReader):
             )
         except ValueError:  # requesting event-epoched data beyond final sample of EEG
             raise exc.MissingDataError(
-                "Unable to load EEG for some events because rel_start or rel_stop parameter " +
-                "is beyond the first or last sample of the EEG recording.  Try loading EEG " +
-                "again after dropping the first or last event(s)."
+                "Requesting EEG beyond the boundaries of the EEG recording. If loading " +
+                "un-epoched EEG, try loading event-epoched EEG. If loading event-epoched " +
+                "EEG, either modify rel_start/rel_stop parameters or try dropping " +
+                "the first or last event(s)."
             )
 
         return data, contacts
